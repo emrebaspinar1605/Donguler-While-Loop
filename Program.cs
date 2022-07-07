2f1fda158877b6b7131fe006d3e0a4cbd5884f98
emrebaspinar1605 <turkuz-elhamdulillah@hotmail.com>
2022-07-07 23:40:25 +0300

    sss

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+System.Console.WriteLine("Bir sayı giriniz: ");
+int sayi=int.Parse(Console.ReadLine());
+int sayac=1;
+int top=0;
+while (sayac<=sayi)
+{
+    top+=sayac;
+    sayac ++;
+}
+System.Console.WriteLine(top/sayi);
+char character='a';
+while (character <= 'z')
+{
+    Console.Write(character);
+    character++;
+}
+
+System.Console.WriteLine("***** Foreach *****");
+string[] cars={"Bmw","Ford","Toyota","Nissan"};
+foreach (var car in cars)
+{
+    System.Console.WriteLine(car);
+}
